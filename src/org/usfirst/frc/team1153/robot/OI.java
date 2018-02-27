36ee6cfbc974d020f467d63816b3034daafdeb6f
Aayush <mathura20@wpsma.org>
2018-02-26 20:12:31 -0500

    Added toggle for Carriage

--- src/org/usfirst/frc/team1153/robot/OI.java
+++ src/org/usfirst/frc/team1153/robot/OI.java
@@ -10,6 +10,7 @@ package org.usfirst.frc.team1153.robot;
 import org.usfirst.frc.team1153.robot.commandGroups.CollectorDownAction;
 import org.usfirst.frc.team1153.robot.commandGroups.CollectorUpAction;
 import org.usfirst.frc.team1153.robot.commands.CarriageDownCommand;
+import org.usfirst.frc.team1153.robot.commands.CarriageToggleCommand;
 import org.usfirst.frc.team1153.robot.commands.CarriageUpCommand;
 import org.usfirst.frc.team1153.robot.commands.ClimberToggleCommand;
 import org.usfirst.frc.team1153.robot.commands.CollectorLeftRightOutCommand;
@@ -38,82 +39,74 @@ public class OI {
 
 	private static final int DRIVER_JOYSTICK = 0;
 	private static final int OPERATOR_STICK = 1;
-	
+
 	public static final int JOYSTICK_LEFT_Y = 1;
 	public static final int JOYSTICK_RIGHT_X = 4;
-	
+
 	private Joystick opStick = new Joystick(OPERATOR_STICK);
-    private Joystick driverStick = new Joystick(DRIVER_JOYSTICK);
-    
-    public Button drTriggerL = new RebelTrigger(driverStick, 2);
-    public Button drTriggerR = new RebelTrigger(driverStick, 3);
-   
-    public Button drButtonY = new JoystickButton(driverStick, 4);
-    public Button drButtonA = new JoystickButton(driverStick, 1);
-    public Button drButtonB = new JoystickButton(driverStick, 2);
-    public Button drButtonX = new JoystickButton(driverStick, 3);
-    
-    public Button drBumperL = new JoystickButton(driverStick, 5);
-    public Button drBumperR = new JoystickButton(driverStick, 6);
-    
-    
-    
-    
-    public Button opTriggerL = new RebelTrigger(opStick, 2);
-    public Button opTriggerR = new RebelTrigger(opStick, 3);
-    
-    public Button opButtonY = new JoystickButton(opStick, 4);
-    public Button opButtonA = new JoystickButton(opStick, 1);
-    public Button opButtonB = new JoystickButton(opStick, 2);
-    public Button opButtonX = new JoystickButton(opStick, 3);
+	private Joystick driverStick = new Joystick(DRIVER_JOYSTICK);
 
-    public Button opBumperL = new JoystickButton(opStick, 5);
-    public Button opBumperR = new JoystickButton(opStick, 6);
+	public Button drTriggerL = new RebelTrigger(driverStick, 2);
+	public Button drTriggerR = new RebelTrigger(driverStick, 3);
 
-    public OI() {
-    	
-    	drTriggerL.whenPressed(new ShiftLowCommand());
-    	drTriggerL.whenReleased(new ShiftHighCommand());
-    	
-    	drButtonY.whenPressed(new PTOToggleCommand());
-    
-    	drButtonA.whenPressed(new ClimberToggleCommand());
+	public Button drButtonY = new JoystickButton(driverStick, 4);
+	public Button drButtonA = new JoystickButton(driverStick, 1);
+	public Button drButtonB = new JoystickButton(driverStick, 2);
+	public Button drButtonX = new JoystickButton(driverStick, 3);
 
-    	opButtonA.whenPressed(new FireShooterCommand());
-    	opButtonA.whenReleased(new RetractShooterCommand());
-    	
-    	opButtonB.whenPressed(new CarriageUpCommand());
-    	opButtonB.whenReleased(new CarriageDownCommand());
-    	
-    	opButtonY.whileHeld(new CollectorOnCommand());
-    	opButtonY.whenReleased(new CollectorOffCommand());
-    	
-    	opButtonX.whileHeld(new CollectorReverseCommand());
-    	opButtonX.whenReleased(new CollectorOffCommand());
-    	
-//    	opButtonY.whileHeld(new CollectorSetRPMCommand());
-//    	opButtonY.whenReleased(new CollectorOffCommand());
-    	
-//    	opTriggerL.whenPressed(new CollectorUpDownInCommand());
-//    	opTriggerL.whenReleased(new CollectorUpDownOutCommand());
-    	
-    	opTriggerR.whenPressed(new CollectorLeftRightToggleCommand());
-    	
-    	opTriggerL.whenPressed(new CollectorUpDownToggleCommand());
-    	
-    	opBumperR.whenPressed(new CollectorDownAction());
-    	opBumperR.whenReleased(new CollectorLeftRightOutCommand());
-    	
-    	opBumperL.whenPressed(new CollectorUpAction());
-    	opBumperL.whenReleased(new CollectorLeftRightOutCommand());
-    }
-    
-    
-    public Joystick getOpStick() {
-    	return opStick;
-    }
-    
-    public Joystick getDriverStick() {
-    	return driverStick;
-    }
+	public Button drBumperL = new JoystickButton(driverStick, 5);
+	public Button drBumperR = new JoystickButton(driverStick, 6);
+
+	public Button opTriggerL = new RebelTrigger(opStick, 2);
+	public Button opTriggerR = new RebelTrigger(opStick, 3);
+
+	public Button opButtonY = new JoystickButton(opStick, 4);
+	public Button opButtonA = new JoystickButton(opStick, 1);
+	public Button opButtonB = new JoystickButton(opStick, 2);
+	public Button opButtonX = new JoystickButton(opStick, 3);
+
+	public Button opBumperL = new JoystickButton(opStick, 5);
+	public Button opBumperR = new JoystickButton(opStick, 6);
+
+	public OI() {
+
+		drTriggerL.whenPressed(new ShiftLowCommand());
+		drTriggerL.whenReleased(new ShiftHighCommand());
+
+		drButtonY.whenPressed(new PTOToggleCommand());
+
+		drButtonA.whenPressed(new ClimberToggleCommand());
+
+		opButtonA.whenPressed(new FireShooterCommand());
+		opButtonA.whenReleased(new RetractShooterCommand());
+
+//		opButtonB.whenPressed(new CarriageUpCommand());
+//		opButtonB.whenReleased(new CarriageDownCommand());
+		
+		opButtonB.whenPressed(new CarriageToggleCommand());
+
+		opButtonY.whileHeld(new CollectorOnCommand());
+		opButtonY.whenReleased(new CollectorOffCommand());
+
+		opButtonX.whileHeld(new CollectorReverseCommand());
+		opButtonX.whenReleased(new CollectorOffCommand());
+
+		opTriggerR.whenPressed(new CollectorLeftRightToggleCommand());
+
+		opTriggerL.whenPressed(new CollectorUpDownToggleCommand());
+
+		opBumperR.whenPressed(new CollectorDownAction());
+		opBumperR.whenReleased(new CollectorLeftRightOutCommand());
+
+		opBumperL.whenPressed(new CollectorUpAction());
+		opBumperL.whenReleased(new CollectorLeftRightOutCommand());
+	}
+
+	public Joystick getOpStick() {
+		return opStick;
+	}
+
+	public Joystick getDriverStick() {
+		return driverStick;
+	}
 }
